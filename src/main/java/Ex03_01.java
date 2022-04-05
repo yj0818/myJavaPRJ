1d6b052534bf7a51960a824524c7b2b8080a873f
yj0818 <kimyejin0818@gmail.com>
2022-04-05 19:28:40 +0900

    2022.04.05 java공부

---
+++ src/main/java/Ex03_01.java
@@ -0,0 +1,8 @@
+public class Ex03_01 {
+    public static void main(String[] args) {
+        System.out.printf("100+100");
+        System.out.printf("\n");
+        System.out.printf("%d", 135 + 1230);
+        System.out.printf("\n");
+    }
+}
---
+++ src/main/java/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.print("안녕하세요?");
+        System.out.print("자바입니다.");
+    }
+
+
+}
